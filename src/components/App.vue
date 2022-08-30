6c5ca55324bb8d9379a9a2ea18b78c50d11692a0
Gleb Bychkov <bychkov-bgo@yandex.ru>
2022-08-30 12:37:34 +0300

    Added started styles for App and created Header component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,31 @@
 <template>
+  <div class="page">
 
+  </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
 
+    }
+  },
+  methods: {
+    
+  }
+}
 </script>
 
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.page {
+  max-width: 1440px;
+  margin: 0 auto;
+  border: 1px solid red;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Добавление товара</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
