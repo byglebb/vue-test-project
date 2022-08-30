7858633fb799280a2cbd2195f5f74cf27f209a0c
Gleb Bychkov <bychkov-bgo@yandex.ru>
2022-08-30 21:38:43 +0300

    Created Header and added to App

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div class="page">
+  <div class="body">
+    <div class="page">
+      <main-header />
+      <div class>
 
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
+import MainHeader from './MainHeader.vue'
 export default {
+  components: {
+    MainHeader
+  },
   data() {
     return {
 
     }
   },
   methods: {
-    
+
   }
 }
 </script>
 
 <style>
+/* @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;900&display=swap'); */
+/* @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;900&display=swap'); */
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;900&display=swap');
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: 'Source Sans Pro', sans-serif;
 }
+
+.body {
+  padding: 32px;
+}
+
 .page {
   max-width: 1440px;
   margin: 0 auto;
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+    <h2 class="header__title">Добавление товара</h2>
+    <select class="header__select">
+      <option class="header__option">По умолчанию</option>
+      <!-- <option class="header__option">По возрастанию цены</option>
+      <option class="header__option">По убыванию цены</option> -->
+    </select>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.header__title {
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 35px;
+  color: #3F3F3F;
+}
+
+.header__select {
+  background: #FFFEFB;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  /* padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 16px; */
+  padding: 10px 16px;
+  border: none;
+}
+
+.header__option {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+}
+</style>
