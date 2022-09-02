dbe05b87f614395c4330abb061aae2943ac5ee5d
Gleb Bychkov <bychkov-bgo@yandex.ru>
2022-09-03 00:55:55 +0300

    added initial card list to the page

--- src/components/AddBar.vue
+++ src/components/AddBar.vue
@@ -38,7 +38,7 @@ export default {
   },
   methods: {
     createCard() {
-      
+      initialCards.name
     }
   }
 }
--- src/components/CardElements.vue
+++ src/components/CardElements.vue
@@ -1,6 +1,16 @@
 <template>
   <section class="elements">
-    <article class="element">
+    <article class="element" v-for="card in initialCards">
+      <button class="element__delete-button" type="button"></button>
+      <img class="element__image" v-bind:src="card.link" v-bind:alt="card.name">
+      <div class="element__description-container">
+        <h2 class="element__name">{{card.name}}</h2>
+        <p class="element__description">{{card.description}}</p>
+        <p class="element__price">{{card.price}}</p>
+      </div>
+    </article>
+
+    <!-- <article class="element">
       <button class="element__delete-button" type="button"></button>
       <img class="element__image" src="../images/Picture.png">
       <div class="element__description-container">
@@ -22,42 +32,61 @@
           интересное</p>
         <p class="element__price">10 000 руб.</p>
       </div>
-    </article>
-
-    <article class="element">
-      <button class="element__delete-button" type="button"></button>
-      <img class="element__image" src="../images/Picture.png">
-      <div class="element__description-container">
-        <h2 class="element__name">Наименование товара Наименование товара</h2>
-        <p class="element__description">Довольно-таки интересное описание товара в несколько строк. Довольно-таки
-          интересное описание товара в несколько строк ки интересное описание товара в несколько строк. Довольно-таки
-          интересное</p>
-        <p class="element__price">10 000 руб.</p>
-      </div>
-    </article>
-    
-    <article class="element">
-    </article>
-    <article class="element">
-    </article>
-    <article class="element">
-    </article>
-    <article class="element">
-    </article>
-    <article class="element">
-    </article>
-    <article class="element">
-    </article>
-    <article class="element">
-    </article>
-    <article class="element">
-    </article>
-    <article class="element">
-    </article>
+    </article> -->
   </section>
 </template>
 
 <script>
+import { initialCards } from './constants.js';
+export default {
+  data() {
+    return {
+      initialCards: [
+        {
+          name: 'Архыз',
+          description: 'Это кавказский хребет Это кавказский хребет Это кавказский хребетЭто кавказский хребет Это кавказский хребет',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+          price: '12 300 руб.'
+        },
+        {
+          name: 'Челябинская область',
+          description: 'А это ЧЕЛЯБИНСК А это ЧЕЛЯБИНСК А это ЧЕЛЯБИНСК',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+          price: '300 руб.'
+        },
+        {
+          name: 'Иваново',
+          description: 'Город невест Город невест Город невестГород невест Город невест Город невест Город невест',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+          price: '300 руб.'
+        },
+        {
+          name: 'Камчатка',
+          description: 'Вулканы, красота: Дорого! Вулканы, красота: Дорого! Вулканы, красота: Дорого!Вулканы, красота: Дорого! Вулканы, красота: Дорого! Вулканы, красота: Дорого!',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+          price: '1 000 000 руб.'
+        },
+        {
+          name: 'Холмогорский район',
+          description: 'Ноу нейм для меня',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+          price: '5 000 руб.'
+        },
+        {
+          name: 'Байкал',
+          description: 'Самое полноводное озеро в мире. Самое полноводное озеро в мире. Самое полноводное озеро в мире.',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+          price: 'Беcценно!'
+        }
+      ]
+    }
+  },
+  methods: {
+    createCard() {
+      console.log(initialCards.name);
+    }
+  }
+}
 
 </script>
 
@@ -137,7 +166,7 @@
   line-height: 20px;
   color: #3F3F3F;
   margin-top: 16px;
-  max-height: 80px;
+  height: 80px;
   /* text-overflow: ellipsis;
   white-space: nowrap; */
   overflow: hidden;
